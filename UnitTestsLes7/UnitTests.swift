34c4eb6eb9966913af8cbfe18641751ac5b2f402
Yaroslav <workbox72@bk.ru>
2023-07-21 18:05:23 +0500

    add UnitTestsLes7

---
+++ UnitTestsLes7/UnitTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import HW_Les_7
+
+class UnitTests: XCTestCase{
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {  
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        self.measure{
+            
+        }
+    }
+
+}
